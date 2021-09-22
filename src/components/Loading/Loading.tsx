a3ddd6b67e8c5b2b5f449750916f4b678fb5fefb
leonace924 <meobius123@gmail.com>
2021-09-22 09:39:05 -0400

    feat: update action sheet library and add basic one on homepage

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Loading = () => {
+const Loading: React.FC = () => {
   return (
     <div className=" flex justify-center items-center">
       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900" />
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import BottomSheet from 'bottom-sheet-react';
+import React, { useRef } from 'react';
+import ActionSheet, { ActionSheetRef } from 'actionsheet-react';
 
 const Home: React.FC = () => {
+  const ref = useRef<ActionSheetRef>();
+  // const [sheetShow, setSheetShow] = useState(false);
+
+  const handleOpen = () => {
+    if (ref) {
+      ref?.current?.open();
+    }
+  };
+
+  const handleClose = () => {
+    if (ref) {
+      ref?.current?.close();
+    }
+  };
+
   return (
     <div>
-      <BottomSheet
-        isExpandable={true}
-        customHeight={200}
-        // onClose={customCloseHandler}
-      >
-        {/* <ChildrenComponent/> */}
-        <h1>test</h1>
-      </BottomSheet>
+      <button onClick={handleOpen}>Open</button>
+      <button onClick={handleClose}>Close</button>
+      <ActionSheet ref={ref}>
+        <div>
+          <h1>test</h1>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+            Button
+          </button>
+        </div>
+      </ActionSheet>
     </div>
   );
 };

520c9c188ecf6bcb26fb496418dc1469d0521536
leonace924 <meobius123@gmail.com>
2021-09-23 01:04:01 -0400

    chore: add custom background color and add asset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './public/index.html',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: '#CC104B',
+      },
+    },
   },
   variants: {
     extend: {},
